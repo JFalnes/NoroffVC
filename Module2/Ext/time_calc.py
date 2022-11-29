33968aa5df3b3cb505c02e9be1ba9adb5bbe9a70
Falnes <johannes@falnes.no>
2022-11-29 13:20:26 +0100

    time_calc added

---
+++ Module2/Ext/time_calc.py
@@ -0,0 +1,14 @@
+import time
+
+num1 = input('Number 1: ')
+num2 = input('Number 2: ')
+
+
+start = time.time()
+
+time_to_sleep = int(num1)/int(num2)
+time.sleep(time_to_sleep)
+end = time.time()
+
+result = end - start
+print('{0:.2f}'.format(result))
